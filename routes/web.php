ee70c2f89a5ea742e59e758cc5d03cf11ffc8af7
Lolpawer <dadu.petrosino@gmail.com>
2022-03-14 17:35:56 +0100

    adds bootstrap and vue

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+
+Route::get('{any?}', function () {
+    return view ('home');
+})->where('any', '.*');
